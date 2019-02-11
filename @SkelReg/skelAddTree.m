3a8e24d8d7e98739968ff6959d086a097672bfa0
Ali Karimi <ali.karimi@brain.mpg.de>
2019-02-11 15:53:18 +0100

    SkelReg.skelAddTree: now accepts more than 1 nml as input

--- @SkelReg/skelAddTree.m
+++ @SkelReg/skelAddTree.m
@@ -1,8 +1,8 @@
 function obj = skelAddTree(obj, nmlFname, modalityType)
 %SKELADDTREE Adds nml as an additional tree to a skeleton stored in the
 %skeleton attribute.
-%   INPUT:  nmlFname: str
-%               Full path to nml file
+%   INPUT:  nmlFname: str/cell
+%               Full path to nml file/s. They should be in cell format
 %           modalityType: str
 %               Modality type of provided nml file. Allowed modalities are:
 %               'fixed' (e.g. EM)
@@ -17,7 +17,13 @@ SkelReg.assertModalityType(modalityType)
 % Assert modality availability in the skeletons property
 obj.assertModalityAvailability(modalityType, 'skeletons')
 
-% Add tree to skeleton modality
-obj.skeletons.(modalityType) = obj.skeletons.(modalityType).addTreeFromSkel(Skeleton(nmlFname));
-
+% Make sure the nmlFnames are in cell format
+if ~iscell(nmlFname)
+    nmlFname={nmlFname};
+end
+for f=1:length(nmlFname)
+    % Add tree to skeleton modality
+    obj.skeletons.(modalityType) = obj.skeletons.(modalityType). ...
+        addTreeFromSkel(Skeleton(nmlFname{f}));
+end
 end
